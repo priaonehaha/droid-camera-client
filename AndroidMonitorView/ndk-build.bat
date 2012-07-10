2587e34da02f5854f0bafcdef69d54a80e7b5423
dianshijin <dianshijin@gmail.com>
2012-07-10 11:47:33 +0000

    添加xvid在cygwin,mingw下编译方法

---
+++ AndroidMonitorView/ndk-build.bat
@@ -0,0 +1,7 @@
+mingw编译xvid方法:
+$ cd /e/droid-camera-client/AndroidMonitorView/
+$ /d/android-ndk-r6b/ndk-build
+
+cygwin编译xvid方法:
+$ cd  /cygdrive/e/droid-camera-client/AndroidMonitorView
+$ /cygdrive/d/android-ndk-r6b/ndk-build
